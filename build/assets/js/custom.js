6920a4c0d3b4e6ea477b89a922fcad71772dbcf5
Sklif79 <sklif_@mail.ru>
2018-04-10 14:14:06 +0300

    add to top button and animation

--- build/assets/js/custom.js
+++ build/assets/js/custom.js
@@ -197,3 +197,61 @@ var EPPZScrollTo = {
         } else return this.parentElement.closest(selector);
     };
 })(Element.prototype);
+
+/**
+ *  Up-Down button
+ * */
+(function () {
+
+    var upDownBtn = document.getElementById('toUp');
+    var check;
+
+    function trackScroll() {
+        var scrolled = window.pageYOffset;
+        var topPosition = 800;
+
+        if (scrolled > topPosition) {
+            upDownBtn.classList.add('js-visible');
+            check = false;
+        }
+
+        if (scrolled > 0 && scrolled < topPosition) {
+            upDownBtn.classList.remove('js-visible');
+        }
+
+        if (scrolled === 0) {
+            check = true;
+        }
+    }
+
+    function backToTop() {
+        upDownBtn.classList.add('js-visible');
+        if (!check) {
+            (function goTop() {
+
+                if (window.pageYOffset !== 0) {
+                    window.scrollBy(0, -80);
+                    setTimeout(goTop, 20);
+                } else {
+                    upDownBtn.classList.remove('js-visible');
+                }
+            })();
+            return;
+        } else if (check) {
+            (function goBottom() {
+                var match = Math.ceil(window.pageYOffset + document.documentElement.clientHeight);
+
+                if (match != document.documentElement.scrollHeight) {
+                    window.scrollBy(0, 80);
+                    setTimeout(goBottom, 0);
+                } else {
+                    upDownBtn.classList.remove('js-visible');
+                }
+            })();
+            return;
+        }
+    }
+
+    window.addEventListener('scroll', trackScroll);
+    upDownBtn.addEventListener('click', backToTop);
+})();
